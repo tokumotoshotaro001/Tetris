5f6482201ec65a94b0afe12598ab0f99dfab7642
tokumotoshotaro001 <138739547+tokumotoshotaro001@users.noreply.github.com>
2023-10-06 12:07:00 +0900

    TS書いた
    TS書いた

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+#include<string.h>
+
+int main(void)
+{
+    char str2[] = { 'u','n','s','i','g','n','e','d',' ','l','o','n','g' };
+    printf("%d", strnlen(str2, 6));
+
+    return 0;
+}
---
+++ Tetris/Tetris/TitleScene.cpp
@@ -0,0 +1,83 @@
+#include "TitleScene.h"
+#include "InputControl.h"
+#include "SceneManrger.h"
+#include "DxLib.h"
+
+/********************************************
+* マクロ定義
+********************************************/
+
+/********************************************
+* 型定義
+********************************************/
+enum
+{
+	E_TITLE_BGM,
+	E_TITLE_CURSOR,
+	E_TITLE_SELECT,
+	E_SOUND_MAX,
+};
+
+/********************************************
+* グローバル変数宣言
+********************************************/
+
+int cursor_numbar;
+int sounds[E_SOUND_MAX];
+
+/********************************************
+* プロトタイプ宣言
+********************************************/
+
+/********************************************
+* タイトル画面：初期化処理
+* 引数：なし
+* 戻り値：エラー情報【－1：異常有、－１以外：正常終了）
+********************************************/
+
+int TitleScene_Initialize(void)
+{
+	int ret = 0;
+	cursor_number = 0;
+	sounds[E_TITLE_BGM] = LoadSoundMem("sound/BGM041.ogg");
+	sounds[E_TITLE_SE_CURSOR] = LoadSoundMem("sound/SE1.mp3");
+	sounds[E_TITLE_SE_SELECT] = LoadSoundMem("sound/SE2.mp3");
+
+	ChangeVolumeSoundMem(120, sounds[E_TITLE_SE_CURSOR]);
+	ChangeVolumeSoundMem(80, sounds[E_TITLE_SE_SELECT]);
+
+	for (int i = 0; i < E_SOUND_MAX; i++)
+	{
+		if (sounds[i] == -1)
+		{
+			ret = -1;
+			break;
+		}
+	}
+	return ret;
+}
+
+/********************************************
+* タイトル画面：更新処理
+* 引数：なし
+* 戻り値：なし
+********************************************/
+void TitleScene_Update(void)
+{
+	//BGMが流れてないときに再生
+	if (CheckSoundMem(sounds[E_TITLE_BGM]) != TRUE)
+	{
+		PlaySoundMem(sounds[E_TITLE_BGM], DX_PLAYTYPE_BACK, FALSE);
+	}
+
+	if (GetButtonDown(XINPUT_BUTTON_B) == TRUE)
+	{
+		StopSoundMem(sounds[E_BUTTON_BGM]);
+		PlaySoundMem(sounds[E_TITLE_SE_SELECT], DX_PLAYTYPE_BASK, FALSE);
+		switch (cursor_number)
+		{
+		case 0:
+
+		}
+	}
+}
