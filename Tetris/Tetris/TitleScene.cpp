fc9e1b3932315699cf2c51e115c511fccfeb1a76
tokumotoshotaro001 <138739547+tokumotoshotaro001@users.noreply.github.com>
2023-10-30 11:18:03 +0900

    GMS書いてる
    GMS書いてる

--- Tetris/Tetris/TitleScene.cpp
+++ Tetris/Tetris/TitleScene.cpp
@@ -77,7 +77,45 @@ void TitleScene_Update(void)
 		switch (cursor_number)
 		{
 		case 0:
+			Change_Scene(E_GAMEMAIN);
+			break;
+		case 1:
+			Change_Scene(E_RANKING);
+			break;
+		case 2:default:
+			Change_Scene(E_END);
+			break;
 
 		}
 	}
+
+	if (GetButtonDown(XINPUT_BUTTON_DPAD_UP) == TRUE)
+	{
+		PlaySoundMem(sounds[E_TITLE_SE_CURSOR], DX_PLAYTYPE_NORMAL, FALSE);
+		if (cursor_number >= 2)
+		{
+			cursor_number = 2;
+		}
+		else
+		{
+			cursor_number++;
+		}
+	}
+}
+
+/********************************************
+* タイトル画面：描画処理
+* 引数：なし
+* 戻り値：なし
+********************************************/
+void TitleScene_Draw(void)
+{
+	SetFontSize(50);
+	DrawString(100, 100, "テトリス", GetColor(255, 255, 255));
+	DrawString(300, 300, "スタート", GetColor(255, 255, 255));
+	DrawString(300, 350, "ランキング", GetColor(255, 255, 255));
+	DrawString(300, 400, "エンド", GetColor(255, 255, 255));
+	SetFontSize(20);
+
+	DrawCircle(275, 325 + (cursor_number * 50), 15, GetColor(255, 0, 0));
 }
